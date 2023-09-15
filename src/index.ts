cb4795cfffb41de03b16209a71322075c3b782ff
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-15 12:26:35 -0300

    atividade2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Olá Kenzie, me chamo Alex!")
+const nameList: string[] = [];
+
+const addName = (name: string): void => {
+    nameList.push(name);
+}
+
+const getNames = (): void => {
+    nameList.forEach(name => console.log(name));
+}
+
+const removeName = (index: number): void => {
+    nameList.splice(index, 1);
+}
+
+const updateName = (index: number, name: string): void => {
+    nameList.splice(index, 1, name);
+}
+
+// Alex, Otávio e Roberto.
+addName("Alex");
+addName("Otávio");
+addName("Roberto");
+removeName(1);
+updateName(1, "Osvaldo");
+getNames();
