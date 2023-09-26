c962abf11587420ec0c7d6afb5ddc2302c4ab692
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-26 15:06:13 -0300

    atividade5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Olá Kenzie, me chamo Alex!")
+import { IProduct } from "./interfaces";
+
+const productList: IProduct[] = [];
+
+const createProduct = (product: IProduct) => {
+    productList.push(product);
+    return { product, message: "Product sucessfully created"};
+}
+
+const getProducts = () => {
+    return productList;
+}
+
+/*
+Geladeira - 2400 - identificador: 1
+Cooktop - 1200 - identificador: 2
+*/
+const product1 = createProduct({ id: 1, name: "Geladeira", price: 2400});
+const product2 = createProduct({ id: 2, name: 'Cooktop', price: 1200});
+console.log(product1);
+console.log(product2);
+console.log(getProducts());
