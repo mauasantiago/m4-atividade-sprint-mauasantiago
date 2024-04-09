16b88023a1585f35973d9cd9a0c93fd56e6e317d
Maua488 <136342770+Maua488@users.noreply.github.com>
2024-04-09 00:30:14 -0300

    Atividade 2 Concluida

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Olá Kenzie, me chamo Mauã!")
+const nameList: string[] = [];
+
+const addName = (name: string): void => {
+    nameList.push(name);
+}
+
+const getNames = (): void => {
+    nameList.forEach(name => console.log(name));
+}
+
+const removeName = (index: number): void => {
+    nameList.splice(index, 1);
+}
+
+const updateName = (index: number, name: string): void => {
+    nameList.splice(index, 1, name);
+}
+
+addName("Alex");
+addName("Otávio");
+addName("Roberto");
+removeName(1);
+updateName(1, "Osvaldo");
+getNames();
